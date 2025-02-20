12726edf2a32153886026321b5d58203afd016ee
RyanDefost <70955105+RyanDefost@users.noreply.github.com>
2025-02-20 14:49:19 +0100

    refactored the code

--- Assets/DragAndDrop.cs
+++ Assets/DragAndDrop.cs
@@ -3,92 +3,78 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
-    private GameObject _currentObject = null;
-    private Collider2D _currentCollider = null;
-    private PuzzleSpawner _spawner;
+    [SerializeField, Range(0, 1)] private float minSnapDistance = 0.25f;
+    [SerializeField, Range(0, 1)] private float maxSnapDistance = 0.75f;
+    private Vector2 minMaxSnapDistance;
 
-    public float gridSize = 1f;
+    private GameObject currentObject = null;
 
-    private float snapDistance = 0.5f;
+    private void Start() => minMaxSnapDistance = new Vector2(minSnapDistance, maxSnapDistance);
 
-    void Start() => _spawner = FindAnyObjectByType<PuzzleSpawner>();
-
-    // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos;
         Vector2 rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+        SetRaycast(rayOrigin);
 
+        UpdatePosition(this.currentObject, rayOrigin);
+    }
+
+    /// <summary>
+    /// Casts a Ray based on if the correct input is given and sets the currentObject
+    /// </summary>
+    /// <param name="rayOrigin"></param>
+    private void SetRaycast(Vector2 rayOrigin)
+    {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             RaycastHit2D hit;
             if (hit = Physics2D.Raycast(rayOrigin, Vector3.forward))
             {
-                if (hit.transform.gameObject.layer != 6)
+                puzzlePiece piece;
+                hit.transform.gameObject.TryGetComponent(out piece);
+
+                if (piece == null)
                     return;
 
-                _currentObject = hit.transform.gameObject;
-                _currentCollider = hit.collider;
+                this.currentObject = hit.transform.gameObject;
             }
         }
+
         if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            _currentObject = null;
-        }
-
-        if (_currentObject != null)
-        {
-            //var posX = RoundToNearestGrid(rayOrigin.x);
-            //var posY = RoundToNearestGrid(rayOrigin.y);
-
-            var posX = MathF.Round(rayOrigin.x);
-            var posY = MathF.Round(rayOrigin.y);
-
-            posX = SnapPos(posX, rayOrigin.x);
-            posY = SnapPos(posY, rayOrigin.y);
-
-            _currentObject.transform.position = new Vector2(posX, posY);
-        }
-        ///
-        //offset = Vector3.zero;
-        //_currentObject.transform.position = rayOrigin;
+            this.currentObject = null;
     }
 
-    private float SnapPos(float pos, float originalPos)
+    /// <summary>
+    /// Sets the position of the currentObject to snap to a grid.
+    /// </summary>
+    /// <param name="currentObject">The current object the player is holding.</param>
+    /// <param name="movePosition">The position the object has at the moment.</param>
+    private void UpdatePosition(GameObject currentObject, Vector2 movePosition)
     {
-        if (originalPos - pos < 0.25)
-        {
-            pos = Mathf.Ceil(pos);
-        }
-        else if (originalPos - pos > 0.75)
-        {
-            pos = Mathf.Floor(pos);
-        }
-        else
-        {
-            pos += 0.5f;
-        }
+        if (currentObject == null)
+            return;
 
-        return pos;
+        float positionX = MathF.Round(movePosition.x);
+        float positionY = MathF.Round(movePosition.y);
+
+        positionX = SnapPos(positionX, movePosition.x, minMaxSnapDistance);
+        positionY = SnapPos(positionY, movePosition.y, minMaxSnapDistance);
+
+        this.currentObject.transform.position = new Vector2(positionX, positionY);
     }
 
-    //Code: https://gamedevbeginner.com/how-to-snap-objects-in-game-in-unity/
-    float RoundToNearestGrid(float pos)
+    /// <summary>
+    /// Tries to snap to a grid, based on the given parameters.
+    /// </summary>
+    /// <returns>returns the position snapped on a grid.</returns>
+    private float SnapPos(float position, float originalPos, Vector2 minMaxDistance)
     {
-        float xDiff = pos % gridSize;
-        bool isPositive = pos > 0 ? true : false;
-        pos -= xDiff;
-        if (Mathf.Abs(xDiff) > (gridSize / 2))
-        {
-            if (isPositive)
-            {
-                pos += gridSize;
-            }
-            else
-            {
-                pos -= gridSize;
-            }
-        }
-        return pos;
+        if (originalPos - position < minMaxDistance.x)
+            return Mathf.Ceil(position);
+
+        if (originalPos - position > minMaxDistance.y)
+            return Mathf.Floor(position);
+
+        return position += 0.5f;
     }
 }
